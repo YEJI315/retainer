bd8b5f3adfd7953e6e52a7542fd1dc08fa504c6f
yeji <kimyeji315@gmail.com>
2025-05-29 17:39:00 +0900

    Initial commit : Spring Boot + Kotlin project with HelloController

---
+++ src/test/kotlin/com/retainer/retainer/RetainerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.retainer.retainer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RetainerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
